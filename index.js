899314f81dae272f0dcb6d2c04a4ef391e90152b
Farouk Abdi <abdifarouk58@gmail.com>
2025-03-12 12:23:41 +0300

    index.js update

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Farouk Abdi";
+const welcomeMessage = "Welcome to Flatbook, Farouk Abdi!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, FAROUK ABDI!";
+const shortGreeting = "Welcome, Farouk!"
